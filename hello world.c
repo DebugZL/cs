5451a5d68b60ae28900af51f0179d8f59f95a1ad
DebugZL <56601907+DebugZL@users.noreply.github.com>
2020-03-20 00:59:07 +0800

    0320-2

--- hello world.c
+++ hello world.c
@@ -6,7 +6,9 @@ int main()
     /* 我的第一个 C 程序 */
     int tree = 1;
     printf("Hello, World! \n");
+    printf("你好! \n");
     tree++;
+    printf("你好!%d \n",tree);
     getchar();
     return 0;
 }
